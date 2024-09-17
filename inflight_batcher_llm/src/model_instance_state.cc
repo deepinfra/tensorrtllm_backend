5f50c1608e37f812d8ed51b259c3ffcf9d8d2509
Patrick Reiter Horn <patrick.horn@gmail.com>
2024-09-17 16:21:56 -0700

    Ugly code to share logit processor state in /dev/shm between the main process and workers

--- inflight_batcher_llm/src/model_instance_state.cc
+++ inflight_batcher_llm/src/model_instance_state.cc
@@ -804,7 +804,7 @@ bool ModelInstanceState::handleStopRequest(TRITONBACKEND_Request* request, std::
 // Split batched TRITONBACKEND_Request into one executor:Request object per sample.
 std::vector<executor::Request> ModelInstanceState::createExecutorRequests(TRITONBACKEND_Request* request,
     bool excludeInputFromOutput, bool isDecoupled, executor::ModelType modelType, bool isOrchestrator,
-    bool specDecFastLogits, std::vector<std::unique_ptr<StructuredLogitProcessorRequestState>> &logitProcessorStates)
+    bool specDecFastLogits, std::vector<std::unique_ptr<FreeStateHolder>> &logitProcessorStates)
 {
     auto inputsTensors = utils::readInputsTensors(request);
     bool streaming = utils::getRequestBooleanInputTensor(request, kStreamingInputTensorName);
@@ -841,7 +841,8 @@ void ModelInstanceState::enqueue(TRITONBACKEND_Request** requests, uint32_t cons
                 continue;
             }
 
-            std::vector<std::unique_ptr<StructuredLogitProcessorRequestState>> logitProcessorStates;
+            std::vector<std::unique_ptr<FreeStateHolder>> logitProcessorStates;
+
             auto executorRequests = createExecutorRequests(request, mInstanceSpecificConfig.excludeInputFromOutput,
                 isDecoupled(), mModelType, mIsOrchestratorMode, mSpeculativeDecodingFastLogits, logitProcessorStates);
 
